7937b086f5f026c3e87be3afb7705cd29d77d444
Piña <25886158+Pineapple6@users.noreply.github.com>
2018-10-14 10:18:13 +0200

    READY FOR DEPLOYING (#12)
    * READY FOR DEPLOYING
    Modified Setup.py and added build distribution
    
    * Remove packaging archives

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup( name = 'extmath',
-       version = '0.9',
-       description = 'Collections of useful tools in mathematics.',
-       author = 'Nattawut Phetmak',
-       license = 'MIT',
-       packages = ['extmath'],
-     )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='extmath',
+    version='0.9',
+    author='Nattawut Phetmak',
+    # author_email='',
+    description='Collections of useful tools in mathematics.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/neizod/extmath',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
